97f6d902ba299c76bf4887f8ba1a16628c9d0d94
ohoh144 <ohoh144@gmail.com>
2019-03-03 18:56:40 +0200

    Add data, add email etc...

--- src/data/data.js
+++ src/data/data.js
@@ -1,375 +1,397 @@
 
 export const commonQ = [
   {
-    question: 'hoe i dovulidation it',
-    answer:"enter the squard ",
-},
-{
-  question: 'hoe i dovulidation it',
-  answer:"enter the squard ",
-},
-{
-  question: 'hoe i dovulidation it',
-  answer:"enter the squard ",
-},
-{
-  question: 'hoe i dovulidation it',
-  answer:"enter the squard ",
-},
-{
-  question: 'hoe i dovulidation it',
-  answer:"enter the squard ",
-}
+    question: 'how i dovulidation it ?',
+    answer: "enter the squard ",
+  },
+  {
+    question: 'how can i do policy ?',
+    answer: "you need to enter the exact prop line ",
+  },
+  {
+    question: 'can i enter scan project by myself ?',
+    answer: "no , just unscan project ",
+  },
+  {
+    question: 'when the project will be ready ? ',
+    answer: " our engeeneir try to do them as fast as posible",
+  },
+  {
+    question: 'can we add the price for faster job done ?',
+    answer: " posible",
+  }
 ]
 
 export const projects = [
-    {
-        name: '090 project',
-        id: 1,//key:value
-        phone: '0506545676',
-        note:"i need it for 23.09",
-        date: '30.10.16',
-        kindData: 'library project',        
-    },
-    {
-        name: 'building project',
-        id: 2,
-        phone: '0506545559',
-        note:"i need it for 23.09",
-        kindData: 'library project',
-        date: '15.10.17'
-    },
-    {
-        name: 'PP project',
-        id: 3,
-        phone: '0506444676',
-        note:"i need it for 23.09",
-        kindData: 'library project',
-        date: '18.04.98'
-    },
-    {
-        name: 'OO project',
-        id: 4,
-        phone: '0506545555',
-        note:"i need it for 23.09",
-        kindData: 'library project',
-        date: '14.05.12'
-    },
-    {
-        name: '99 project',
-        id: 5,
-        phone: '0506545222',
-        note:"i need it for 23.09",
-        kindData: 'library project',
-        date: '11.10.03'
-    },
-    {
-        name: 'gogo project',
-        id: 6,
-        phone: '0505555555',
-        note:"i need it for 23.09",
-        kindData: 'library project',
-        date: '08.10.92'
-    },
-    {
-        name: 'LOL project',
-        id: 7,
-        phone: '0506545252',
-        note:"i need it for 23.09",
-        kindData: 'library project',
-        date: '05.06.98'
-    },
-    {
-        name: '90 project',
-        id: 8,
-        phone: '0506545222',
-        note:"i need it for 23.09",
-        kindData: 'library project',
-        date: '02.05.98'
-    },
-    {
-        name: '20 project',
-        id: 9,
-        phone: '0506545222',
-        note:"i need it for 23.09",
-        kindData: 'library project',
-        date: '06.10.98'
-    },
-    {
-        name: '00 project',
-        id: 10,
-        phone: '0506545222',
-        note:"i need it for 23.09",
-        kindData: 'library project',
-        date: '16.04.78'
-    },
-    {
-        name: '565 project',
-        id: 11,
-        phone: '0506555556',
-        note:"i need it for 23.09",
-        kindData: 'library project',
-        date: '07.09.13'
-    },
-    
-];    
+  {
+    name: '090 project',
+    id: 1,//key:value
+    phone: '0506545676',
+    note: "i need it for 23.09",
+    memberDate: '30.10.16',
+    kindData: 'library project',
+    date: '30.10.16',
+    email: 'BB@gmail.com'
+  },
+  {
+    name: 'building project',
+    id: 2,
+    phone: '0506545559',
+    note: "i need it for 23.09",
+    memberDate: '30.10.16',
+    kindData: 'library project',
+    date: '15.10.17',
+    email: 'BB@gmail.com'
+  },
+  {
+    name: 'PP project',
+    id: 3,
+    phone: '0506444676',
+    note: "i need it for 23.09",
+    memberDate: '30.10.16',
+    kindData: 'library project',
+    date: '18.04.98',
+    email: 'BB@gmail.com'
+  },
+  {
+    name: 'OO project',
+    id: 4,
+    phone: '0506545555',
+    note: "i need it for 23.09",
+    memberDate: '30.10.16',
+    kindData: 'library project',
+    date: '14.05.12',
+    email: 'BB@gmail.com'
+  },
+  {
+    name: '99 project',
+    id: 5,
+    phone: '0506545222',
+    note: "i need it for 23.09",
+    memberDate: '30.10.16',
+    kindData: 'library project',
+    date: '11.10.03',
+    email: 'BB@gmail.com'
+  },
+  {
+    name: 'gogo project',
+    id: 6,
+    phone: '0505555555',
+    note: "i need it for 23.09",
+    memberDate: '30.10.16',
+    kindData: 'library project',
+    date: '08.10.92',
+    email: 'BB@gmail.com'
+  },
+  {
+    name: 'LOL project',
+    id: 7,
+    phone: '0506545252',
+    note: "i need it for 23.09",
+    memberDate: '30.10.16',
+    kindData: 'library project',
+    date: '05.06.98',
+    email: 'BB@gmail.com'
+  },
+  {
+    name: '90 project',
+    id: 8,
+    phone: '0506545222',
+    note: "i need it for 23.09",
+    memberDate: '30.10.16',
+    kindData: 'library project',
+    date: '02.05.98',
+    email: 'BB@gmail.com'
+  },
+  {
+    name: '20 project',
+    id: 9,
+    phone: '0506545222',
+    note: "i need it for 23.09",
+    memberDate: '30.10.16',
+    kindData: 'library project',
+    date: '06.10.98',
+    email: 'BB@gmail.com'
+  },
+  {
+    name: '00 project',
+    id: 10,
+    phone: '0506545222',
+    note: "i need it for 23.09",
+    memberDate: '30.10.16',
+    kindData: 'library project',
+    date: '16.04.78',
+    email: 'BB@gmail.com'
+  },
+  {
+    name: '565 project',
+    id: 11,
+    phone: '0506555556',
+    note: "i need it for 23.09",
+    memberDate: '30.10.16',
+    kindData: 'library project',
+    date: '07.09.13',
+    email: 'BB@gmail.com'
+  },
+
+];
 
 export const objects = [
-    {
-        question: 'are you familiar with literary logic?'
-    },
-    {
-        question: 'Prefer logical problems?'
-    },
-    {
-        question: 'Do you speak business English?'
-    },
-    {
-        question: 'Do You know this programming language for over a year'
-    },
-    {
-        question: 'Do you work fulltime job?'
-    },
-    {
-        question: 'Do you speak another language?'
-    },
-    {
-        question: 'Do you knoe Design?'
-    },
-    {
-        question: 'Are u thr administr?'
-    },
-    
+  {
+    question: 'are you familiar with literary logic?'
+  },
+  {
+    question: 'Prefer logical problems?'
+  },
+  {
+    question: 'Do you speak business English?'
+  },
+  {
+    question: 'Do You know this programming language for over a year'
+  },
+  {
+    question: 'Do you work fulltime job?'
+  },
+  {
+    question: 'Do you speak another language?'
+  },
+  {
+    question: 'Do you knoe Design?'
+  },
+  {
+    question: 'Are u thr administr?'
+  },
+
 ];
- export const graphs = [
-    {
-      id: "japan",
-      color: "hsl(231, 70%, 50%)",
-      data: [
-        {
-          "x": "plane",
-          "y": 268
-        },
-        {
-          "x": "helicopter",
-          "y": 275
-        },
-        {
-          "x": "boat",
-          "y": 68
-        },
-        {
-          "x": "train",
-          "y": 229
-        },
-        {
-          "x": "subway",
-          "y": 262
-        },
-        {
-          "x": "bus",
-          "y": 104
-        },
-        {
-          "x": "car",
-          "y": 111
-        },
-        {
-          "x": "moto",
-          "y": 27
-        },
-        {
-          "x": "bicycle",
-          "y": 98
-        },
-        {
-          "x": "others",
-          "y": 238
-        }
-      ]
-    },
-    {
-      id: "france",
-      color: "hsl(221, 70%, 50%)",
-      data: [
-        {
-          "x": "plane",
-          "y": 150
-        },
-        {
-          "x": "helicopter",
-          "y": 146
-        },
-        {
-          "x": "boat",
-          "y": 192
-        },
-        {
-          "x": "train",
-          "y": 137
-        },
-        {
-          "x": "subway",
-          "y": 285
-        },
-        {
-          "x": "bus",
-          "y": 179
-        },
-        {
-          "x": "car",
-          "y": 19
-        },
-        {
-          "x": "moto",
-          "y": 101
-        },
-        {
-          "x": "bicycle",
-          "y": 49
-        },
-        {
-          "x": "others",
-          "y": 292
-        }
-      ]
-    },
-    {
-      id: "us",
-      color: "hsl(146, 70%, 50%)",
-      data: [
-        {
-          "x": "plane",
-          "y": 271
-        },
-        {
-          "x": "helicopter",
-          "y": 10
-        },
-        {
-          "x": "boat",
-          "y": 192
-        },
-        {
-          "x": "train",
-          "y": 150
-        },
-        {
-          "x": "subway",
-          "y": 3
-        },
-        {
-          "x": "bus",
-          "y": 33
-        },
-        {
-          "x": "car",
-          "y": 91
-        },
-        {
-          "x": "moto",
-          "y": 80
-        },
-        {
-          "x": "bicycle",
-          "y": 190
-        },
-        {
-          "x": "others",
-          "y": 131
-        }
-      ]
-    },
-    {
-      id: "germany",
-      color: "hsl(184, 70%, 50%)",
-      data: [
-        {
-          "x": "plane",
-          "y": 95
-        },
-        {
-          "x": "helicopter",
-          "y": 173
-        },
-        {
-          "x": "boat",
-          "y": 32
-        },
-        {
-          "x": "train",
-          "y": 46
-        },
-        {
-          "x": "subway",
-          "y": 154
-        },
-        {
-          "x": "bus",
-          "y": 257
-        },
-        {
-          "x": "car",
-          "y": 42
-        },
-        {
-          "x": "moto",
-          "y": 223
-        },
-        {
-          "x": "bicycle",
-          "y": 259
-        },
-        {
-          "x": "others",
-          "y": 233
-        }
-      ]
-    },
-    {
-      id: "norway",
-      color: "hsl(119, 70%, 50%)",
-      data: [
-        {
-          "x": "plane",
-          "y": 255
-        },
-        {
-          "x": "helicopter",
-          "y": 21
-        },
-        {
-          "x": "boat",
-          "y": 103
-        },
-        {
-          "x": "train",
-          "y": 58
-        },
-        {
-          "x": "subway",
-          "y": 199
-        },
-        {
-          "x": "bus",
-          "y": 90
-        },
-        {
-          "x": "car",
-          "y": 107
-        },
-        {
-          "x": "moto",
-          "y": 189
-        },
-        {
-          "x": "bicycle",
-          "y": 137
-        },
-        {
-          "x": "others",
-          "y": 25
-        }
-      ]
-    }
-  ]
+export const graphs = [
+  {
+    id: "japan",
+    color: "hsl(231, 70%, 50%)",
+    data: [
+      {
+        "x": "plane",
+        "y": 268
+      },
+      {
+        "x": "helicopter",
+        "y": 275
+      },
+      {
+        "x": "boat",
+        "y": 68
+      },
+      {
+        "x": "train",
+        "y": 229
+      },
+      {
+        "x": "subway",
+        "y": 262
+      },
+      {
+        "x": "bus",
+        "y": 104
+      },
+      {
+        "x": "car",
+        "y": 111
+      },
+      {
+        "x": "moto",
+        "y": 27
+      },
+      {
+        "x": "bicycle",
+        "y": 98
+      },
+      {
+        "x": "others",
+        "y": 238
+      }
+    ]
+  },
+  {
+    id: "france",
+    color: "hsl(221, 70%, 50%)",
+    data: [
+      {
+        "x": "plane",
+        "y": 150
+      },
+      {
+        "x": "helicopter",
+        "y": 146
+      },
+      {
+        "x": "boat",
+        "y": 192
+      },
+      {
+        "x": "train",
+        "y": 137
+      },
+      {
+        "x": "subway",
+        "y": 285
+      },
+      {
+        "x": "bus",
+        "y": 179
+      },
+      {
+        "x": "car",
+        "y": 19
+      },
+      {
+        "x": "moto",
+        "y": 101
+      },
+      {
+        "x": "bicycle",
+        "y": 49
+      },
+      {
+        "x": "others",
+        "y": 292
+      }
+    ]
+  },
+  {
+    id: "us",
+    color: "hsl(146, 70%, 50%)",
+    data: [
+      {
+        "x": "plane",
+        "y": 271
+      },
+      {
+        "x": "helicopter",
+        "y": 10
+      },
+      {
+        "x": "boat",
+        "y": 192
+      },
+      {
+        "x": "train",
+        "y": 150
+      },
+      {
+        "x": "subway",
+        "y": 3
+      },
+      {
+        "x": "bus",
+        "y": 33
+      },
+      {
+        "x": "car",
+        "y": 91
+      },
+      {
+        "x": "moto",
+        "y": 80
+      },
+      {
+        "x": "bicycle",
+        "y": 190
+      },
+      {
+        "x": "others",
+        "y": 131
+      }
+    ]
+  },
+  {
+    id: "germany",
+    color: "hsl(184, 70%, 50%)",
+    data: [
+      {
+        "x": "plane",
+        "y": 95
+      },
+      {
+        "x": "helicopter",
+        "y": 173
+      },
+      {
+        "x": "boat",
+        "y": 32
+      },
+      {
+        "x": "train",
+        "y": 46
+      },
+      {
+        "x": "subway",
+        "y": 154
+      },
+      {
+        "x": "bus",
+        "y": 257
+      },
+      {
+        "x": "car",
+        "y": 42
+      },
+      {
+        "x": "moto",
+        "y": 223
+      },
+      {
+        "x": "bicycle",
+        "y": 259
+      },
+      {
+        "x": "others",
+        "y": 233
+      }
+    ]
+  },
+  {
+    id: "norway",
+    color: "hsl(119, 70%, 50%)",
+    data: [
+      {
+        "x": "plane",
+        "y": 255
+      },
+      {
+        "x": "helicopter",
+        "y": 21
+      },
+      {
+        "x": "boat",
+        "y": 103
+      },
+      {
+        "x": "train",
+        "y": 58
+      },
+      {
+        "x": "subway",
+        "y": 199
+      },
+      {
+        "x": "bus",
+        "y": 90
+      },
+      {
+        "x": "car",
+        "y": 107
+      },
+      {
+        "x": "moto",
+        "y": 189
+      },
+      {
+        "x": "bicycle",
+        "y": 137
+      },
+      {
+        "x": "others",
+        "y": 25
+      }
+    ]
+  }
+]
